b01232bd3210519bc1cd70930f79994833d11b7a
David Rodriguez <rodriguezd11@uabc.edu.mx>
2022-03-04 13:07:25 -0800

    Conversion de caracteres a enteros

---
+++ Conversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena;
+            int num1 = 5;
+            int num2, resultado;
+
+            //Preguntamos al usuario el numero
+            Console.Write("Dame un numero para sumarlo: ");
+            //Se asigna la variable de tipo string
+            cadena = Console.ReadLine();
+            //num2 = Convert.ToInt32(cadena);
+            
+            //Parse al recibir valor nulo crea una excepcion
+            num2 = Int32.Parse(cadena);
+            //Realizar la suma
+            resultado = num1 + num2;
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
